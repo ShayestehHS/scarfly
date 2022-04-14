8e11ff06606b4521427f1890b9aef7e41edade33
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-04-14 23:27:05 +0430

    Update STATIC_URL and MEDIA_URL

--- backend/core/settings/production.py
+++ backend/core/settings/production.py
@@ -7,8 +7,8 @@ ALLOWED_HOSTS = ['0.0.0.0', 'scarfly.ir', 'api.scarfly.ir', 'www.scarfly.ir', 'w
 
 STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'media'
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = '/api/static/'
+MEDIA_URL = '/api/media/'
 
 INSTALLED_APPS += ['corsheaders', 'admin_honeypot']
 MIDDLEWARE += ['corsheaders.middleware.CorsMiddleware', ]
