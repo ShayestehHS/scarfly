c4cb0356b559df18fae0ab838792a800be745e76
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-04-27 15:55:08 +0430

    Add new conversation handler for getting the tracking code from Order table.

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -94,6 +94,44 @@ def status_end(update: Update, context: CallbackContext) -> int:
     return ConversationHandler.END
 
 
+def tracking_code(update: Update, context: CallbackContext):
+    logger.info(f"Function: tracking_code     User:{update.message.from_user.username}")
+    update.message.reply_text(text="کد رهگیری ای که ما به شما دادیم را وارد کنید...")
+    return STATUS_SECOND
+
+
+def tracking_code_get_payment_id(update: Update, context: CallbackContext) -> int:
+    chat_id = update.message.chat_id
+    if not check_user_validation(chat_id):
+        update.message.reply_text("شما در ابتدا باید من را فعال کنید تا شما رو بشناسم و از شما دستور بگیرم.")
+        activate_user(chat_id, update)
+        return ConversationHandler.END
+    payment_id = update.message.text
+    is_valid = validate_payment_id(payment_id)
+    if not is_valid:
+        update.message.reply_text("این کد از نظر من درست نیست.\nلطفا دوباره کد رو بررسی کن و برام بفرست.\nاگر هم میخوای میتونی این فرایند رو متوقف کنی.\n /cancel")
+        return STATUS_SECOND
+
+    subquery = f"SELECT id FROM accounts_user as au WHERE au.chat_id = '{chat_id}'"
+    query = f"SELECT tracking_code FROM orders_order " \
+            f"INNER JOIN accounts_user au on orders_order.user_id = au.id " \
+            f"WHERE orders_order.payment_id = '{payment_id}' " \
+            f"AND orders_order.user_id = ({subquery})"
+    with Connection() as conn:
+        cur = conn.cursor()
+        cur.execute(query)
+        results = cur.fetchall()
+    if len(results) == 0:
+        update.message.reply_text('کد وارد شده، صحیح نمی باشد.\nلطفا کد وارد شده را بررسی نمایید و دوباره ارسال کنید.\nدر غیر اینصورت فرایند جاری را متوقف کنید.\n /cancel')
+        return STATUS_SECOND
+
+    update.message.reply_text("شناسه ي مرسوله ي شما:"
+                              f"\n {results[0]['tracking_code']}")
+    update.message.reply_text("\n برای مشاهده ی وضعیت مرسوله خود در وبسایت شرکت پست، روی لینک زیر کلیک کنید:"
+                              f"\n https://tracking.post.ir/search.aspx?id={results[0]['tracking_code']}")
+    return ConversationHandler.END
+
+
 def cancel(update: Update, context: CallbackContext) -> int:
     logger.info(f"Function: cancel            User:{update.message.from_user.username}")
     update.message.reply_text('فرمان شما دریافت شد.\n'
@@ -107,7 +145,7 @@ def main() -> None:
     updater = Updater(bot_token)
     dispatcher = updater.dispatcher
 
-    conv_handler = ConversationHandler(
+    status_conv_handler = ConversationHandler(
         entry_points=[CommandHandler('status', status)],
         states={
             STATUS_FIRST: [MessageHandler(Filters.text & ~Filters.command, status)],
@@ -115,7 +153,16 @@ def main() -> None:
         },
         fallbacks=[CommandHandler('cancel', cancel)],
     )
-    dispatcher.add_handler(conv_handler)
+    tracking_code_conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('tracking_code', status)],
+        states={
+            STATUS_FIRST: [MessageHandler(Filters.text & ~Filters.command, tracking_code)],
+            STATUS_SECOND: [MessageHandler(Filters.text & ~Filters.command, tracking_code_get_payment_id)]
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+    dispatcher.add_handler(status_conv_handler)
+    dispatcher.add_handler(tracking_code_conv_handler)
     dispatcher.add_handler(CommandHandler('start', start))
 
     updater.start_polling()
