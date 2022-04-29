d45192d3f907ca47228d5756601fa2020ddb2d40
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-04-30 00:55:54 +0430

    Create 404 page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,23 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './components/PaymentResult/PaymentResult';
 import axios from 'axios';
 import Product from './components/product/Product';
-
+import PageNotFound from './components/404/404-page'
 
 if (window.location.origin === "http://localhost:3000") {
-  axios.defaults.baseURL = "http://127.0.0.1:8000";
+    axios.defaults.baseURL = "http://127.0.0.1:8000";
 } else {
-  axios.defaults.baseURL = window.location.origin;
+    axios.defaults.baseURL = window.location.origin;
 }
 
 
-
 function App() {
-  return (
-    <Routes>
-      <Route path='/orders/verify/' element={<Home/>} />
-      <Route path='/products/:productID/' element={<Product/>} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path='/orders/verify/' element={<Home/>}/>
+            <Route path='/products/:productID/' element={<Product/>}/>
+            <Route path='*' element={<PageNotFound/>}/>
+        </Routes>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/404/404-page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PageNotFound = () => {
+    return (
+        <div id="wrapper" dir='ltr' className='border-box'>
+            <div className="number">404</div>
+            <div id="info">
+                <h3>This page could not be found</h3>
+            </div>
+        </div >
+    )
+}
+
+export default PageNotFound
