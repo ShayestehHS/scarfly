19ab1853c140162388f4a31f4138bfc69edfbc0f
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-05-18 14:24:31 +0430

    Minor code cleaning.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,20 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
-import { Toaster } from 'react-hot-toast';
+import {BrowserRouter} from 'react-router-dom'
+import {Toaster} from 'react-hot-toast';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
- 
-    <Toaster
-  position="top-center"
-  reverseOrder={false}
-/>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <Toaster position="top-center" reverseOrder={false}/>
+            <App/>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
