e5712b139372874e58f1ac55728b3d6587e6b6a1
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2022-05-19 22:14:17 +0430

    Set message for OnlyOrderOfUser permission.

--- backend/orders/permissions.py
+++ backend/orders/permissions.py
@@ -2,5 +2,7 @@ from rest_framework.permissions import BasePermission
 
 
 class OnlyOrderOfUser(BasePermission):
+    message = "You can't view results of other users"
+
     def has_object_permission(self, request, view, obj):
         return obj.user_id == request.user.id
